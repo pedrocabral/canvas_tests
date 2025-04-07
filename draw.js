ca00ea39c33c4906627a4f31b8a625223d8eb811
pedrocabral <pedrocabral@users.noreply.github.com>
2025-04-07 01:40:13 -0300

    Create draw.js

---
+++ draw.js
@@ -0,0 +1,9 @@
+  const canvas = document.getElementById("screen");
+  const ctx = canvas.getContext("2d");
+  let radius = canvas.height / 2;
+  ctx.translate(radius, radius);
+  radius = radius * 0.80;
+    
+  ctx.arc(0, 0, radius, 0 , 2 * Math.PI);
+  ctx.strokeStyle = "black";
+  ctx.stroke;
