da49d69217e2188f22f11b477258c22045938c06
pedrocabral <pedrocabral@users.noreply.github.com>
2025-04-07 02:09:33 -0300

    Update draw.js

--- draw.js
+++ draw.js
@@ -3,11 +3,11 @@ window.addEventListener("load", draw, true);
 function draw() {
   const canvas = document.getElementById("screen");
   const ctx = canvas.getContext("2d");
-  ctx.beginPath();
-  ctx.moveTo(20,20);
-  ctx.strokeStyle = "red";
-  ctx.lineTo(180,20);
-  ctx.stroke();
+  // ctx.beginPath();
+  // ctx.moveTo(20,20);
+  // ctx.strokeStyle = "red";
+  // ctx.lineTo(180,20);
+  // ctx.stroke();
   
   let radius = canvas.height / 2;
   ctx.translate(radius, radius);
