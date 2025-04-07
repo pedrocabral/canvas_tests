9fe9295e93d9605bf6cd89211fc69a0538d4e1ab
pedrocabral <pedrocabral@users.noreply.github.com>
2025-04-07 02:06:32 -0300

    Update draw.js

--- draw.js
+++ draw.js
@@ -7,12 +7,12 @@ function draw() {
   ctx.moveTo(20,20);
   ctx.strokeStyle = "red";
   ctx.lineTo(180,20);
-  ctx.stroke;
+  ctx.stroke();
   
   let radius = canvas.height / 2;
   ctx.translate(radius, radius);
   radius = radius * 0.80;
   ctx.arc(0, 0, radius, 0 , 2 * Math.PI);
   ctx.strokeStyle = "black";
-  ctx.stroke;
+  ctx.stroke();
 }
